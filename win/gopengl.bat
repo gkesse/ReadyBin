77f3a66e3a8f2e5b5c4e06b462873468baee1704
Gerard KESSE <tiakagerard@hotmail.com>
2019-11-01 11:20:32 +0100

    Initial Commit

--- win/gopengl.bat
+++ win/gopengl.bat
@@ -3,8 +3,6 @@
 set PATH=C:\Windows\System32
 set PATH=C:\Users\Admin\Downloads\Programs\ReadyBin\win;%PATH%
 set PATH=C:\MinGW\bin;%PATH%
-set PATH=C:\Program Files\Java\jdk-11.0.1\bin;%PATH%
-set PATH=C:\Users\Admin\Downloads\eclipse-cpp-2019-03-R-win32-x86_64\eclipse;%PATH%
 ::===============================================
 set GGIT="C:\Program Files\Git\bin\git.exe"
 set GPROJECT=C:\Users\Admin\Downloads\Programs\ReadyOpenGL
@@ -15,36 +13,72 @@ set GPWD=%cd%
 set GPROCESS="%1"
 ::===============================================
 if %GPROCESS% == "compile" (
-    cd %GBUILD%
-    mingw32-make
+    goto :compile
 ) else ( 
 if %GPROCESS% == "clean" (
-    cd %GBUILD%
-    mingw32-make clean
+    goto :clean
 ) else ( 
-if %GPROCESS% == "eclipse" (
-    cd %GBUILD%
-    eclipse
+if %GPROCESS% == "git_push_all" (
+    goto :git_push_all
 ) else ( 
-if %GPROCESS% == "git_status" (
-    cd %GPROJECT%
-    %GGIT% add --all
+if %GPROCESS% == "git_pull" (
+    goto :git_pull
 ) else ( 
-if %GPROCESS% == "git_add" (
-    cd %GPROJECT%
-    %GGIT% add --all
-) else ( 
-if %GPROCESS% == "git_commit" (
-    cd %GPROJECT%
-    %GGIT% commit -m "Initial Commit"
-) else ( 
-if %GPROCESS% == "git_push" (
-    cd %GPROJECT%
-    %GGIT% push -u origin master
-) else ( 
-    cd %GBIN%
-    GProject.exe %*
-)))))))
+    goto :run
+))))
 ::===============================================
-cd %GPWD%
+:: OPENGL
+::===============================================
+:compile
+    cd %GBUILD%
+    ::-----------------------------------------------
+    mingw32-make
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
+::===============================================
+:clean
+    cd %GBUILD%
+    ::-----------------------------------------------
+    mingw32-make clean
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
+::===============================================
+:run
+    cd %GBIN%
+    ::-----------------------------------------------
+    GProject.exe %*
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
+::===============================================
+:: GIT
+::===============================================
+:git_push_all
+    cd %GPROJECT%
+    ::-----------------------------------------------
+    %GGIT% add --all
+    %GGIT% commit -m "Initial Commit"
+    %GGIT% push -u origin master
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
+::===============================================
+:git_pull
+    cd %GPROJECT%
+    ::-----------------------------------------------
+    %GGIT% pull
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
+::===============================================
+:: ERROR
+::===============================================
+:error
+    ::-----------------------------------------------
+    echo ERREUR: Nom du processus ?
+    ::-----------------------------------------------
+    cd %GPWD%
+goto :eof
 ::===============================================
