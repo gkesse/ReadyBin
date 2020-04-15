f7326684069d7e6d242a0015286e8616175a0489
Pierre YURI <pierre.yuri@outlook.fr>
2020-04-15 08:28:45 +0200

    Initial Commit

---
+++ win/.config.bat
@@ -0,0 +1,11 @@
+@echo off 
+::===============================================
+set "GBIN_PATH=C:\Users\Admin\Downloads\Programs\ReadyBin"
+set "GCPP_BUILD=C:\Users\Admin\Downloads\Programs\ReadyCpp\win"
+set "GJAVA_HOME=C:\Program Files\Java\jdk-11.0.1\bin"
+set "GMUPARSER_BIN=C:\Users\Admin\Downloads\Programs\ReadyLib\dev\muparser\mingw\bin"
+set "GMINGW_BIN=C:\MinGW\bin"
+set "GMINGW_BIN_64=C:\Users\Admin\Downloads\x86_64-8.1.0-release-win32-sjlj-rt_v6-rev0\mingw64\bin"
+set "GCMAKE_BIN=C:\Program Files\CMake\bin"
+set "GCYGWIN_BIN=C:\cygwin\bin"
+::===============================================
