72ea6893c3961946cf3263fb463a925737c3056f
Pierre YURI <pierre.yuri@outlook.fr>
2020-06-20 23:46:34 +0200

    Initial Commit

--- win/gz_vbox_catalina.bat
+++ win/gz_vbox_catalina.bat
@@ -1,25 +1,29 @@
 @echo off
 ::===============================================
-set PATH=C:\Program Files\Oracle\VirtualBox.%PATH%
+set "GPWD=%cd%"
 ::===============================================
-set "GVBOX_MACHINE=iMacOsX"
+if "%GPATH%" == "" ( echo ERREUR : GPATH ? & goto :eof ) & if not exist %GPATH% ( echo ERREUR : GPATH ? & goto :eof )
 ::===============================================
-set "GTAB=    "
+call %GPATH%/.config.bat
+::===============================================
+if "%GVBOX_MACHINE%" == "" ( echo ERREUR : GVBOX_MACHINE ? & goto :eof )
+::===============================================
+set "lTmpFile_01=%GPATH%\tmp\tmp_file_01.txt"
 ::===============================================
 :: Configurations
-echo.
-echo Configurations :
-echo %GTAB%GVBOX_MACHINE    : %GVBOX_MACHINE%
-echo %GTAB%UUUUUUU          : PPPPPPPPPP
-echo.
+printf "\n"
+printf "Configurations :\n"
+printf "\t%%-20s : %%s\n" "GVBOX_MACHINE" "%GVBOX_MACHINE%"
+printf "\n"
 ::===============================================
-set /p lAnswer= "Confirmation de continuer(Oui|[N]on) ? : " 
-echo %lAnswer%
+set /p "lAnswer=Confirmation de continuer (Oui|[N]on) ? : " || set "lAnswer=n"
 set "lAnswerKey=%lAnswer:~0,1%"
-set "lAnswerIn=%lAnswer:~0,1%"
-echo %lChar%
-goto :eof
+echo %lAnswerKey%| tr '[:upper:]' '[:lower:]'>%lTmpFile_01%
+set /p lAnswerIn=<%lTmpFile_01%
+if not "%lAnswerIn%" == "o" ( echo NON : operation annulee & goto :eof ) 
+echo OUI : operation effectuee
 ::===============================================
+echo.
 VBoxManage.exe modifyvm "%GVBOX_MACHINE%" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
 VBoxManage setextradata "%GVBOX_MACHINE%" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
 VBoxManage setextradata "%GVBOX_MACHINE%" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
