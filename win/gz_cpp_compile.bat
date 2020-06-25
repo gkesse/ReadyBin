0be7dd86f6cd08460d40c3dd6e0cff122b981700
Pierre YURI <pierre.yuri@outlook.fr>
2020-06-25 07:56:44 +0200

    Initial Commit

--- win/gz_cpp_compile.bat
+++ win/gz_cpp_compile.bat
@@ -6,7 +6,7 @@ if "%GPATH%" == "" ( echo ERREUR : GPATH ? & goto :eof ) & if not exist %GPATH% 
 ::===============================================
 call %GPATH%/.config.bat
 ::===============================================
-if "%GC_BUILD%" == "" ( echo ERREUR : GC_BUILD ? & goto :eof ) & if not exist %GC_BUILD% ( echo ERREUR : GC_BUILD ? & goto :eof )
+if "%GCPP_BUILD%" == "" ( echo ERREUR : GCPP_BUILD ? & goto :eof ) & if not exist %GCPP_BUILD% ( echo ERREUR : GCPP_BUILD ? & goto :eof )
 if "%GMINGW_PATH%" == "" ( echo ERREUR : GMINGW_PATH ? & goto :eof ) & if not exist %GMINGW_PATH% ( echo ERREUR : GMINGW_PATH ? & goto :eof )
 ::===============================================
 set "PATH=%GMINGW_PATH%;%PATH%"
@@ -16,7 +16,7 @@ set "lTmpFile_01=%GPATH%\tmp\tmp_file_01.txt"
 :: Configurations
 printf "\n"
 printf "Configurations :\n"
-printf "\t%%-20s : %%s\n" "GC_BUILD" "%GC_BUILD%"
+printf "\t%%-20s : %%s\n" "GCPP_BUILD" "%GCPP_BUILD%"
 printf "\t%%-20s : %%s\n" "GMINGW_PATH" "%GMINGW_PATH%"
 printf "\n"
 ::===============================================
@@ -28,8 +28,8 @@ if not "%lAnswerIn%" == "o" ( echo NON : operation annulee & goto :eof )
 echo OUI : operation effectuee
 ::===============================================
 echo.
-cd %GC_BUILD%
-set "lBinFile=bin\gp_c.exe"
+cd %GCPP_BUILD%
+set "lBinFile=bin\gp_cpp.exe"
 if exist "%lBinFile%" ( del %lBinFile% )
 mingw32-make argv="%*"
 echo.
