71afd7b130fb64e6843673a51dc462ca6a53b8c8
Gerard KESSE <tiakagerard@hotmail.com>
2020-08-16 15:06:24 +0200

    Initial Commit

--- win/gz_cpp_run.bat
+++ win/gz_cpp_run.bat
@@ -15,6 +15,8 @@ if "%G_STATE%" == "S_ADMIN" ( goto :GCpp_ADMIN
 ) else ( if "%G_STATE%" == "S_CHOICE" ( goto :GCpp_CHOICE
 ) else ( if "%G_STATE%" == "S_CPP_COMPILE_CPP_PATH" ( goto :GCpp_CPP_COMPILE_CPP_PATH
 ) else ( if "%G_STATE%" == "S_CPP_COMPILE_CPP_NAME" ( goto :GCpp_CPP_COMPILE_CPP_NAME
+) else ( if "%G_STATE%" == "S_CPP_COMPILE_CPP_COMPILO" ( goto :GCpp_CPP_COMPILE_CPP_COMPILO
+) else ( if "%G_STATE%" == "S_CPP_COMPILE_CPP_TARGET" ( goto :GCpp_CPP_COMPILE_CPP_TARGET
 ) else ( if "%G_STATE%" == "S_CPP_COMPILE" ( goto :GCpp_CPP_COMPILE
 ) else ( if "%G_STATE%" == "S_CPP_RUN_CPP_PATH" ( goto :GCpp_CPP_RUN_CPP_PATH
 ) else ( if "%G_STATE%" == "S_CPP_RUN_CPP_NAME" ( goto :GCpp_CPP_RUN_CPP_NAME
@@ -23,7 +25,7 @@ if "%G_STATE%" == "S_ADMIN" ( goto :GCpp_ADMIN
 ) else ( if "%G_STATE%" == "S_LOAD" ( goto :GCpp_LOAD
 ) else ( if "%G_STATE%" == "S_QUIT" ( goto :GCpp_QUIT
 ) else ( goto :eof
-)))))))))))))
+)))))))))))))))
 goto :GCpp_Main
 ::===============================================
 :GCpp_ADMIN
@@ -37,6 +39,7 @@ printf "CPP !!!\n"
 printf "\t%%-2s : %%s\n" "-q" "quitter l'application"
 printf "\t%%-2s : %%s\n" "-i" "reinitialiser l'application"
 printf "\t%%-2s : %%s\n" "-a" "redemarrer l'application"
+printf "\t%%-2s : %%s\n" "-v" "valider les configurations"
 printf "\n"
 set "G_STATE=S_LOAD"
 goto :GCpp_Main
@@ -44,7 +47,6 @@ goto :GCpp_Main
 :GCpp_METHOD
 printf "CPP :\n"
 printf "\t%%-2s : %%s\n" "1" "compiler un projet Cpp"
-printf "\t%%-2s : %%s\n" "2" "executer un projet Cpp"
 printf "\n"
 set "G_STATE=S_CHOICE"
 goto :GCpp_Main
@@ -57,8 +59,7 @@ if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
 ) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
 ) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
 ) else ( if "%lAnswer%" == "1" ( set "G_STATE=S_CPP_COMPILE_CPP_PATH" & set "G_CPP_ID=%lAnswer%" 
-) else ( if "%lAnswer%" == "2" ( set "G_STATE=S_CPP_RUN_CPP_PATH" & set "G_CPP_ID=%lAnswer%" 
-)))))
+))))
 goto :GCpp_Main
 ::===============================================
 :GCpp_CPP_COMPILE_CPP_PATH
@@ -68,8 +69,9 @@ if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_PATH%" )
 if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
 ) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
 ) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
+) else ( if "%lAnswer%" == "-v" ( set "G_STATE=S_CPP_COMPILE"
 ) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_COMPILE_CPP_NAME" & set "G_CPP_PATH=%lAnswer%" 
-))))
+)))))
 goto :GCpp_Main
 ::===============================================
 :GCpp_CPP_COMPILE_CPP_NAME
@@ -79,46 +81,39 @@ if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_NAME%" )
 if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
 ) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
 ) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
-) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_COMPILE" & set "G_CPP_NAME=%lAnswer%" 
+) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_COMPILE_CPP_COMPILO" & set "G_CPP_NAME=%lAnswer%" 
+))))
+goto :GCpp_Main
+::===============================================
+:GCpp_CPP_COMPILE_CPP_COMPILO
+set "lAnswer=" 
+set /p lAnswer=G_CPP_COMPILO (%G_CPP_COMPILO%) ? : 
+if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_COMPILO%" )
+if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
+) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
+) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
+) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_COMPILE_CPP_TARGET" & set "G_CPP_COMPILO=%lAnswer%" 
+))))
+goto :GCpp_Main
+::===============================================
+:GCpp_CPP_COMPILE_CPP_TARGET
+set "lAnswer=" 
+set /p lAnswer=G_CPP_TARGET (%G_CPP_TARGET%) ? : 
+if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_TARGET%" )
+if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
+) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
+) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
+) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_COMPILE" & set "G_CPP_TARGET=%lAnswer%" 
 ))))
 goto :GCpp_Main
 ::===============================================
 :GCpp_CPP_COMPILE
 echo.
 set "lBuldPath=%G_CPP_PATH%\%G_CPP_NAME%\win"
+set "lMakefile=Makefile"
+if not "%G_CPP_COMPILO%" == "" ( set "lMakefile=Makefile.%G_CPP_COMPILO%" )
 cd %lBuldPath%
-mingw32-make
-echo.
-set "G_STATE=S_SAVE"
-goto :GCpp_Main
-::===============================================
-:GCpp_CPP_RUN_CPP_PATH
-set "lAnswer=" 
-set /p lAnswer=G_CPP_PATH (%G_CPP_PATH%) ? : 
-if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_PATH%" )
-if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
-) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
-) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
-) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_RUN_CPP_NAME" & set "G_CPP_PATH=%lAnswer%" 
-))))
-goto :GCpp_Main
-::===============================================
-:GCpp_CPP_RUN_CPP_NAME
-set "lAnswer=" 
-set /p lAnswer=G_CPP_NAME (%G_CPP_NAME%) ? : 
-if "%lAnswer%" == "" ( set "lAnswer=%G_CPP_NAME%" )
-if "%lAnswer%" == "-q" ( set "G_STATE=S_END"
-) else ( if "%lAnswer%" == "-i" ( set "G_STATE=S_INIT" 
-) else ( if "%lAnswer%" == "-a" ( set "G_STATE=S_ADMIN"
-) else ( if not "%lAnswer%" == "" ( set "G_STATE=S_CPP_RUN" & set "G_CPP_NAME=%lAnswer%" 
-))))
-goto :GCpp_Main
-::===============================================
-:GCpp_CPP_RUN
-echo.
-set "lBuldPath=%G_CPP_PATH%\%G_CPP_NAME%\win"
-cd %lBuldPath%
-make
+mingw32-make -f %lMakefile% %G_CPP_TARGET%
 echo.
 set "G_STATE=S_SAVE"
 goto :GCpp_Main
@@ -127,6 +122,8 @@ goto :GCpp_Main
 call gz_process_in sqlite_config_save "G_CPP_ID" "%G_CPP_ID%"
 call gz_process_in sqlite_config_save "G_CPP_PATH" "%G_CPP_PATH%"
 call gz_process_in sqlite_config_save "G_CPP_NAME" "%G_CPP_NAME%"
+call gz_process_in sqlite_config_save "G_CPP_COMPILO" "%G_CPP_COMPILO%"
+call gz_process_in sqlite_config_save "G_CPP_TARGET" "%G_CPP_TARGET%"
 set "G_STATE=S_QUIT" & cd %GPWD%
 goto :GCpp_Main
 ::===============================================
@@ -134,6 +131,8 @@ goto :GCpp_Main
 call gz_process_in sqlite_config_load "G_CPP_ID" G_CPP_ID
 call gz_process_in sqlite_config_load "G_CPP_PATH" G_CPP_PATH
 call gz_process_in sqlite_config_load "G_CPP_NAME" G_CPP_NAME
+call gz_process_in sqlite_config_load "G_CPP_COMPILO" G_CPP_COMPILO
+call gz_process_in sqlite_config_load "G_CPP_TARGET" G_CPP_TARGET
 set "G_STATE=S_METHOD"
 goto :GCpp_Main
 ::===============================================
